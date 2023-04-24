865641bf7620d3f5a854076b5a3ca89bfa6a8b23
Killer <alanchik99@gmail.com>
2023-04-24 22:36:03 +0300

    react-slick component (yarn & npm package was used

--- src/entites/books-club/ui/books-club/index.tsx
+++ src/entites/books-club/ui/books-club/index.tsx
@@ -1,23 +1,26 @@
 import { useCallback } from "react";
 import { useNavigate } from "react-router";
-import { useAppSelector } from "shared/api"
-import { Slider } from "shared/ui";
+import { useAppSelector } from "shared/api";
 import { ClubCard } from "../club-card";
 
+import SlickSlider from "shared/ui/slider/slickSlider";
+
 export const BooksClubItem = ({ title }: { title: string }) => {
-    const { clubs } = useAppSelector(state => state.books_club);
-    const navigate = useNavigate()
+  const { clubs } = useAppSelector((state) => state.books_club);
+  const navigate = useNavigate();
 
-    const handleClick = useCallback((id: number) => {
-        navigate(`/club/${id}`)
-    }, [])
+  const handleClick = useCallback((id: number) => {
+    navigate(`/club/${id}`);
+  }, []);
 
-    return <div className="club__slider_item">
-        <h2 className="club__subtitle">{title}</h2>
-        <Slider slidesToShow={2}>
-            {clubs.map(el => (
-                <ClubCard data={el} key={el.id} onClick={handleClick} />
-            ))}
-        </Slider>
+  return (
+    <div className="club__slider_item">
+      <h2 className="club__subtitle">{title}</h2>
+      <SlickSlider slidesToShow={3}>
+        {clubs.map((el) => (
+          <ClubCard data={el} key={el.id} onClick={handleClick} />
+        ))}
+      </SlickSlider>
     </div>
-}
+  );
+};
--- src/entites/books-club/ui/characteristic/index.tsx
+++ src/entites/books-club/ui/characteristic/index.tsx
@@ -1,17 +1,23 @@
-import { useAppSelector } from "shared/api"
-import { Slider } from "shared/ui"
-
+import { useAppSelector } from "shared/api";
+import SlickSlider from "shared/ui/slider/slickSlider";
+// import { Slider } from "shared/ui"
 
 export const BooksCharacteristics = ({ title }: { title: string }) => {
-    const { categories } = useAppSelector(state => state.books_club);
+  const { categories } = useAppSelector((state) => state.books_club);
 
-    return <div className="club__slider_item">
-        <h2 className="club__subtitle">{title}</h2>
+  return (
+    <div className="club__slider_item">
+      <h2 className="club__subtitle">{title}</h2>
 
-        <Slider slidesToShow={4}>
-            {categories.map((el, index) => {
-                return <div className="club__slider_category" key={index}>{el}</div>
-            })}
-        </Slider>
+      <SlickSlider slidesToShow={4}>
+        {categories.map((el, index) => {
+          return (
+            <div className="club__slider_category" key={index}>
+              {el}
+            </div>
+          );
+        })}
+      </SlickSlider>
     </div>
-}
+  );
+};
